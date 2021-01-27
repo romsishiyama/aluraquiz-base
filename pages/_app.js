c4750e2809dd0e982c4227fcfe99d7b9f8966a25
Osmar dos Santos Neto <osmar.neto@multilog.com.br>
2021-01-26 23:47:24 -0300

    ajustes _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Head from 'next/head';
@@ -42,6 +43,7 @@ export default function App({ Component, pageProps }) {
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
       </Head>
       <ThemeProvider theme={theme}>
+        {/* eslint-disable react/jsx-props-no-spreading */}
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
